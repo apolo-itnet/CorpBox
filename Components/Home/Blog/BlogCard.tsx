b96a770f66eaeb2a67043645d33da61f92e1e75a
Apolo Barua Apurbo <apolo.itnet@gmail.com>
2026-04-15 22:18:02 +0600

    Blog & Contact commit

--- Components/Home/Blog/BlogCard.tsx
+++ Components/Home/Blog/BlogCard.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import { BiCalendar } from "react-icons/bi";
 import { FaRegFolderOpen } from "react-icons/fa6";
+import { FiArrowDownRight } from "react-icons/fi";
 
 type Props = {
   image: string;
@@ -24,10 +25,26 @@ const BlogCard = ({ image, title }: Props) => {
           {/* Date */}
           <div className="flex items-center space-x-3">
             <BiCalendar className="text-red-600 w-6 h-6" />
-            <p className="text-gray-600 font-semibold text-sm">April 15, 2023</p>
+            <p className="text-gray-600 font-semibold text-sm">
+              April 15, 2023
+            </p>
           </div>
         </div>
-        <h3 className="text-md font-bold mt-4">{title}</h3>
+        {/* Blog Title */}
+        <h1 className="text-lg font-bold mt-4 text-gray-900 hover:text-orange-500 transition-all duration-300 cursor-pointer">
+          {title}
+        </h1>
+        {/* Read More Button */}
+        <button className="relative inline-block mt-8 cursor-pointer group">
+          {/* Button Text */}
+          <span className="inline-flex items-center gap-2 text-sm font-semibold tracking-[2px] uppercase text-gray-800">
+            Read More
+            {/* Arrow Icon */}
+            <FiArrowDownRight className="text-orange-500 text-xl transition-transform duration-300 group-hover:translate-x-1" />
+          </span>
+          {/* Underline */}
+          <span className="block w-full h-px group-hover:w-0 transition-all duration-300 bg-orange-500 mt-2"></span>
+        </button>
       </div>
     </div>
   );
---
+++ Components/Home/Contact/Contact.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { FaArrowRight } from "react-icons/fa6";
+
+const Contact = () => {
+  return (
+    <div className="pt-16 pb-16 bg-gray-200">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 w-[90%] lg:w*-[80%] items-center mx-auto mt-16">
+        {/* Contact Form */}
+        <div className="lg:col-span-2 bg-gray-900 rounded-xl p-6">
+          {/* Name Input */}
+          <input
+            type="text"
+            placeholder="Name"
+            className="border-2  border-gray-600 w-full px-6 py-3 rounded-full mb-6 placeholder:text-white text-white"
+          />
+          {/* Email Input */}
+          <input
+            type="email"
+            placeholder="Email"
+            className="border-2 border-gray-600 w-full px-6 py-3 rounded-full mb-6 placeholder:text-white text-white"
+          />
+          {/* Message Input */}
+          <input
+            type="text"
+            placeholder="Subject"
+            className="border-2 border-gray-600 w-full px-6 py-3 rounded-full mb-6 placeholder:text-white text-white"
+          />
+          {/* Message Input */}
+          <textarea
+            placeholder="Message"
+            className="border-2 border-gray-600 w-full px-6 py-3 rounded-lg mb-6 placeholder:text-white text-white"
+            rows={4}
+          />
+          {/* Submit Button */}
+          <button className="w-full bg-orange-300 hover:bg-orange-500 text-white font-bold py-3 px-6 rounded-full cursor-pointer transition duration-300 uppercase">
+            Send Message
+          </button>
+        </div>
+        {/* Contact Card */}
+        <div className="lg:col-span-1">
+          {/* Contact Number */}
+          <div className=" p-6 mt-4 mb-4 bg-blue-500 flex items-center space-x-4">
+            <div className="w-16 h-16 rounded-full bg-orange-400 flex items-center justify-center flex-col">
+              <FaArrowRight className="text-white" />
+            </div>
+            <div>
+              <h1 className="text-white text-md font-semibold">Call Now</h1>
+              <h2 className="text-white font-bold text-lg">+880 1823 456789</h2>
+            </div>
+          </div>
+          {/* Email */}
+          <div className=" p-6 mt-4 mb-4  bg-blue-500 flex items-center space-x-4">
+            <div className="w-16 h-16 rounded-full bg-orange-400 flex items-center justify-center flex-col">
+              <FaArrowRight className="text-white" />
+            </div>
+            <div>
+              <h1 className="text-white text-md font-semibold">Email</h1>
+              <h2 className="text-white font-bold text-lg">example@email.com</h2>
+            </div>
+          </div>
+          {/* Address */}
+          <div className=" p-6 mt-4 mb-4  bg-blue-500 flex items-center space-x-4">
+            <div className="w-16 h-16 rounded-full bg-orange-400 flex items-center justify-center flex-col">
+              <FaArrowRight className="text-white" />
+            </div>
+            <div>
+              <h1 className="text-white text-md font-semibold">Address</h1>
+              <h2 className="text-white font-bold text-lg">123 Main Street, City, Country</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
